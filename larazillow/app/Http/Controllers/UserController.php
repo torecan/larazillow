a9cd5ceb506e548ef74285a7819e2f939b527855
Törecan Celik <toerecan.celik@check24.de>
2023-10-10 20:28:11 +0200

    userContoller methods

--- larazillow/app/Http/Controllers/UserController.php
+++ larazillow/app/Http/Controllers/UserController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function update()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function destroy()
+    {
+        //
+    }
 }
